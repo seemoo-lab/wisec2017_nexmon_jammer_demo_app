4d7bd4b8a9fe1cb9686356b26c30a69860d7f281
Efstathios Deligeorgopoulos <edeligeorgopoulos@seemoo.tu-darmstadt.de>
2017-05-09 17:35:34 +0200

    working app

--- WiFiJammer/app/src/test/java/de/seemoo/nexmon/jammer/ExampleUnitTest.java
+++ WiFiJammer/app/src/test/java/de/seemoo/nexmon/jammer/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package seemo.wifijammer;
+package de.seemoo.nexmon.jammer;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
